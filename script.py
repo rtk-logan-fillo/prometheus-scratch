8b211c10fa4309cd18791260398fe1d5613ee323
Logan Fillo <logan.fillo@arcticwolf.com>
2023-07-06 22:03:14 +0000

    working

--- script.py
+++ script.py
@@ -1,7 +1,48 @@
 from urllib.parse import urlparse, quote_plus
+import ssl
 import random
-import time
+from urllib.request import build_opener, HTTPHandler, HTTPSHandler,Request
+
 from prometheus_client import CollectorRegistry, Gauge, Counter, Enum,  push_to_gateway
+
+################################################################################
+# Below is copied from the source code and modified to prevent SSL verification
+################################################################################
+def default_handler(
+        url: str,
+        method: str,
+        timeout,
+        headers,
+        data: bytes,
+):
+    """Default handler that implements HTTP/HTTPS connections.
+
+    Used by the push_to_gateway functions. Can be re-used by other handlers."""
+
+    return _make_handler(url, method, timeout, headers, data, HTTPHandler)
+
+def _make_handler(
+        url: str,
+        method: str,
+        timeout,
+        headers,
+        data: bytes,
+        base_handler
+):
+    def handle() -> None:
+        request = Request(url, data=data, unverifiable=True)
+        request.get_method = lambda: method  # type: ignore
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+        for k, v in headers:
+            request.add_header(k, v)
+        resp = build_opener(HTTPSHandler(context=context)).open(request, timeout=timeout)
+        if resp.code >= 400:
+            raise OSError(f"error talking to pushgateway: {resp.code} {resp.msg}")
+    return handle
+################################################################################
+
 
 cids = ['baconsecurity', 'baconsecurity', 'baconsecurity','baconsecurity' ]
 dids = ['baconsecurity45', 'baconsecurity45', 'baconsecurity45' , 'baconsecurity67']
@@ -44,6 +85,6 @@ for i in range(4):
     http2 = Gauge("endpoint_http_status_code2", "Endpoint HTTP Status Codes", ['customer_id', 'deployment_id', 'integration', 'url', 'host', 'path'], registry=registry)
     http2.labels(cid, did, integration, url, host, path).set(code)
 
-    push_to_gateway('localhost:9091', job=job, registry=registry)
+    push_to_gateway('https://push.metrik.infra.us001-dev.rtkwlf.io', job=job, registry=registry, handler=default_handler)
 
 print("done")
